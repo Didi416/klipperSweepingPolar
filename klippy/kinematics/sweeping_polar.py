105aeaf560f9d2f097e1ca97e70427d64ccf04c5
Darkmonkeyz <darkmonkeyzgaming@gmail.com>
2026-07-03 12:44:02 +1000

    experimentalhomingsupport

--- klippy/kinematics/sweeping_polar.py
+++ klippy/kinematics/sweeping_polar.py
@@ -4,7 +4,7 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging, math
-import stepper
+import stepper,  mathutil, chelper
 
 
 def distance_to_center(p1, p2):
@@ -29,10 +29,10 @@ def distance_to_center(p1, p2):
 class SweepingPolarKinematics:
     def __init__(self, toolhead, config):
         # Setup axis steppers
-        stepper_bed = stepper.PrinterStepper(config.getsection('stepper_bed'),
+        stepper_bed = stepper.LookupMultiRail(config.getsection('stepper_bed'),
                                              units_in_radians=True)
         #
-        stepper_arm = stepper.PrinterStepper(config.getsection('stepper_arm'), units_in_radians=True)
+        stepper_arm = stepper.LookupMultiRail(config.getsection('stepper_arm'), units_in_radians=True)
 
         self.distfrombed = distfrombed = config.getfloat('distfrombed', above=0.)
         #
@@ -40,9 +40,8 @@ class SweepingPolarKinematics:
         stepper_bed.setup_itersolve('sweeping_polar_stepper_alloc', b'a', distfrombed)
         stepper_arm.setup_itersolve('sweeping_polar_stepper_alloc', b'r', distfrombed)
         rail_z.setup_itersolve('cartesian_stepper_alloc', b'z')
-        self.rails = [rail_z]
-        self.steppers = [stepper_bed, stepper_arm] + [ s for r in self.rails
-                                          for s in r.get_steppers() ]
+        self.rails = [stepper_bed, stepper_arm, rail_z]
+    
         for s in self.get_steppers():
             s.set_trapq(toolhead.get_trapq())
         # Setup boundary checks
@@ -56,21 +55,34 @@ class SweepingPolarKinematics:
             'max_angular_velocity', above=0., default=0)
         self.limit_z = (1.0, -1.0)
         self.limit_xy2 = -1.
-        self.limit_z = self.rails[0].get_range()
+        self.limit_z = self.rails[2].get_range()
         self.limit_xy2 = self.distfrombed**2
-        #max_xy = self.rails[0].get_range()[1]
-        min_z, max_z = self.rails[0].get_range()
+        #max_xy = self.rails[2].get_range()[1]
+        min_z, max_z = self.rails[2].get_range()
         self.axes_min = toolhead.Coord((-distfrombed, -distfrombed, min_z))
         self.axes_max = toolhead.Coord((distfrombed, distfrombed, max_z))
         self.set_position([0., 0., 0.], "")
+
+
+
+        # Homing trickery fake cartesial kinematic - Stolen from https://github.com/bondus/5barscara/blob/master/FiveBarElbowKlipper/five_bar_elbow.py#L95
+        self.printer = config.get_printer()
+        ffi_main, ffi_lib = chelper.get_ffi()
+        self.cartesian_kinematics_L = ffi_main.gc(
+            ffi_lib.cartesian_stepper_alloc('x'), ffi_lib.free)
+        self.cartesian_kinematics_R = ffi_main.gc(
+            ffi_lib.cartesian_stepper_alloc('y'), ffi_lib.free)
+
+
+
     def get_steppers(self):
-        return list(self.steppers)
+        return [s for rail in self.rails for s in rail.get_steppers()]
     
 
     def calc_position(self, stepper_positions):
-        bed_angle = stepper_positions[self.steppers[0].get_name()]
-        arm_angle = stepper_positions[self.steppers[1].get_name()]
-        z_pos = stepper_positions[self.rails[0].get_name()]
+        bed_angle = stepper_positions[self.rails[0].get_name()]
+        arm_angle = stepper_positions[self.rails[1].get_name()]
+        z_pos = stepper_positions[self.rails[2].get_name()]
         #
         #
         #
@@ -88,8 +100,70 @@ class SweepingPolarKinematics:
 
     def home(self, homing_state):
         # XXX - homing not implemented
-        homing_state.set_axes([0, 1, 2])
-        homing_state.set_homed_position([0., 0., 0.])
+        axes = homing_state.get_axes()
+        
+        if 0 in axes or 1 in axes: #  XY
+            # Home left and right at the same time
+            self.homedXY = False
+            homing_state.set_axes([0, 1])
+            rails = [self.rails[0], self.rails[1]]
+            l_endstop = rails[0].get_homing_info().position_endstop
+            l_min, l_max = rails[0].get_range()
+
+            r_endstop = rails[1].get_homing_info().position_endstop
+            r_min, r_max = rails[1].get_range()
+
+            # Swap to linear kinematics
+            toolhead = self.printer.lookup_object('toolhead')
+            toolhead.flush_step_generation()
+
+            steppers = self.get_arm_steppers()
+            kinematics = [self.cartesian_kinematics_L,
+                          self.cartesian_kinematics_R]
+            prev_sks    = [stepper.set_stepper_kinematics(kinematic)
+                            for stepper, kinematic in zip(steppers, kinematics)]
+
+            try:
+                homepos  = [l_endstop, r_endstop, None, None]
+                hil = rails[0].get_homing_info()
+                if hil.positive_dir:
+                    forcepos = [0, 0, None, None]
+                else:
+                    forcepos = [l_max, r_max, None, None]
+                
+
+                homing_state.home_rails(rails, forcepos, homepos)
+                #swap back to sweeping polar 
+                for stepper, prev_sk in zip(steppers, prev_sks):
+                    stepper.set_stepper_kinematics(prev_sk)
+                #code to convert the fake cartesian position to the correct position. for now it will default to 0,0
+                [x,y] = [0,0]
+                toolhead.set_position( [x, y, 0, 0], (0, 1))
+                toolhead.flush_step_generation()
+                #self.homedXY = True
+                logging.info("Homed LR done")
+
+            except Exception as e:
+                for stepper, prev_sk in zip(steppers, prev_sks):
+                    stepper.set_stepper_kinematics(prev_sk)
+                toolhead.flush_step_generation()
+                raise
+
+        
+        if 2 in axes: # Z
+            logging.info("5be home Z %s", axes)
+            rail = self.rails[2]
+            position_min, position_max = rail.get_range()
+            hi = rail.get_homing_info()
+            homepos = [None, None, None]
+            homepos[2] = hi.position_endstop
+            forcepos = list(homepos)
+            if hi.positive_dir:
+                forcepos[2] -= 1.5 * (hi.position_endstop - position_min)
+            else:
+                forcepos[2] += 1.5 * (position_max - hi.position_endstop)
+            
+            homing_state.home_rails([rail], forcepos, homepos)
 
     def check_move(self, move):
         if move.axes_d[0] or move.axes_d[1]:
